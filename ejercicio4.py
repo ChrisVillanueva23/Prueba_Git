d0a20d06429b5edae06c3ee5d3d1631c9b42e683
ChrisVillanueva23 <cv935026@gmail.com>
2024-07-09 11:49:36 -0600

    Agregando el ejercicio 4

---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+num1 = int(input("Ingrese un numero entero: "))
+num2 = int(input("Ingrese un numero entero: "))
+resultado = num1 * num2
+
+print("El resultado de la operaciòn es: "+str(resultado))
